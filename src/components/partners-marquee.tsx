69d757df19742b52b0c6c1514be4c241fe4cbc3d
Dreake Ntimama <leyiantempes@gmail.com>
2025-07-30 08:50:28 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <PartnersMarquee>

--- src/components/partners-marquee.tsx
+++ src/components/partners-marquee.tsx
@@ -5,14 +5,14 @@ import Image from 'next/image';
 import { cn } from '@/lib/utils';
 
 const partners = [
-  { name: 'TIBS', logo: 'https://placehold.co/158x48.png', hint: 'TIBS logo' },
-  { name: 'Zoho', logo: 'https://placehold.co/158x48.png', hint: 'Zoho logo' },
-  { name: 'ICANN', logo: 'https://placehold.co/158x48.png', hint: 'ICANN logo' },
-  { name: 'AfTLD', logo: 'https://placehold.co/158x48.png', hint: 'AfTLD logo' },
-  { name: 'Communications Authority of Kenya', logo: 'https://placehold.co/158x48.png', hint: 'CA Kenya logo' },
-  { name: 'ICT Authority', logo: 'https://placehold.co/158x48.png', hint: 'ICTA logo' },
-  { name: 'Eveminet', logo: 'https://placehold.co/158x48.png', hint: 'Eveminet logo' },
-  { name: 'Elimu Holdings', logo: 'https://placehold.co/158x48.png', hint: 'Elimu Holdings logo' },
+  { name: 'TIBS', logo: '/partners/TIBS.png', hint: 'TIBS logo' },
+  { name: 'Zoho', logo: '/partners/Zoho.png', hint: 'Zoho logo' },
+  { name: 'ICANN', logo: '/partners/ICANN.png', hint: 'ICANN logo' },
+  { name: 'AfTLD', logo: '/partners/AfTLD.png', hint: 'AfTLD logo' },
+  { name: 'Communications Authority of Kenya', logo: '/partners/CA.png', hint: 'CA Kenya logo' },
+  { name: 'ICT Authority', logo: '/partners/ICTA.png', hint: 'ICTA logo' },
+  { name: 'Eveminet', logo: '/partners/Eveminet.png', hint: 'Eveminet logo' },
+  { name: 'Elimu', logo: '/partners/Elimu.png', hint: 'Elimu Holdings logo' },
 ];
 
 export function PartnersMarquee() {
