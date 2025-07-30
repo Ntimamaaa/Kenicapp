399e95a525c1dc497a043e6fa24798e918b629bc
Dreake Ntimama <leyiantempes@gmail.com>
2025-07-30 13:10:40 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <LicensedRegistrar

--- src/app/(pages)/registrars/licensed/page.tsx
+++ src/app/(pages)/registrars/licensed/page.tsx
@@ -187,7 +187,7 @@ export default function LicensedRegistrarsPage() {
         </div>
       </section>
     
-      <div className="bg-muted">
+      <div className="bg-background">
         <div className="container mx-auto max-w-7xl py-12 px-4 md:px-6">
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
             {filteredRegistrars.map((registrar) => (
