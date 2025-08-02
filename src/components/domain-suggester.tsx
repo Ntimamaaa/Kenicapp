354d6fcd9980a6cb1a3cd792da3dec994f703722
Dreake Ntimama <leyiantempes@gmail.com>
2025-08-02 08:06:06 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <DomainSuggester>

--- src/components/domain-suggester.tsx
+++ src/components/domain-suggester.tsx
@@ -102,7 +102,6 @@ export function DomainSuggester() {
     }
   }
 
-
   return (
     <Card className="w-full overflow-hidden">
         <CardContent className="p-0">
@@ -129,7 +128,7 @@ export function DomainSuggester() {
               </form>
             </div>
             <div className="bg-secondary p-6">
-                <div className={cn("flex flex-col h-full min-h-[270px]")}>
+                <div className="flex flex-col h-full min-h-[270px]">
                     {displayedSuggestions && displayedSuggestions.length > 0 ? (
                     <>
                         <div className="flex justify-between items-center mb-2">
